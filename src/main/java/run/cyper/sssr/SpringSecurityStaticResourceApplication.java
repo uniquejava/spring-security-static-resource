e8f1c4531b72c9f96ec7d17163b9150ddf0d71f2
uniquejava <uniquejava@gmail.com>
2024-01-30 16:40:55 +0800

    initial commit

---
+++ src/main/java/run/cyper/sssr/SpringSecurityStaticResourceApplication.java
@@ -0,0 +1,13 @@
+package run.cyper.sssr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityStaticResourceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityStaticResourceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/run/cyper/sssr/SpringSecurityStaticResourceApplicationTests.java
@@ -0,0 +1,13 @@
+package run.cyper.sssr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityStaticResourceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
